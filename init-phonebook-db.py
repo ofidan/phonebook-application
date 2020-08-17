a6575cfb18831dd2c2e791541e3302c1970f6fe1
Oguzhan Fidan <ofidan5435@gmail.com>
2020-08-17 22:29:28 +0300

    new

--- init-phonebook-db.py
+++ init-phonebook-db.py
@@ -2,15 +2,14 @@ import mysql.connector
 from mysql.connector import errorcode
 
 config = {
-  'user': 'admin',
-  'password': 'Callahan_1',
-  'host': 'phonebook-app-db.cbanmzptkrzf.us-east-1.rds.amazonaws.com',
-  'database': 'phonebook',
-  'raise_on_warnings': True
+    'user': 'admin',
+    'password': 'Callahan_1',
+    'host': 'phonebook-db.cbanmzptkrzf.us-east-1.rds.amazonaws.com',
+    'database': 'phonebook',
+    'raise_on_warnings': True
 }
 
-# Write a function named `init_phonebook_db` which initializes the phonebook db
-# Create phonebook table within mysql db and populate with sample data
+
 def init_phonebook_db(cursor):
     drop_table = 'DROP TABLE IF EXISTS phonebook.phonebook;'
     phonebook_table = """
@@ -28,21 +27,23 @@ def init_phonebook_db(cursor):
         ("Sergio Taco", "67854"),
         ("Vincenzo Altobelli", "876543554");
     """
+
     cursor.execute(drop_table)
     cursor.execute(phonebook_table)
     cursor.execute(data)
 
+
 try:
-  cnx = mysql.connector.connect(**config)
-  init_phonebook_db(cnx.cursor(buffered=True))
-  cnx.commit()
+    cnx = mysql.connector.connect(**config)
+    init_phonebook_db(cnx.cursor(buffered=True))
+    cnx.commit()
 except mysql.connector.Error as err:
-  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-    print("Something is wrong with your user name or password")
-  elif err.errno == errorcode.ER_BAD_DB_ERROR:
-    print("Database does not exist")
-  else:
-    print(err)
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("Something is wrong with the username or password")
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("Database does not exits.")
+    else:
+        print(err)
 else:
-  print("Phonebook table created and populated with successfully")
-  cnx.close()
+    print("Phonebook table created and populated successfull")
+    cnx.close()
